c1571a34f36d843acd49e356a8832653999d001a
OR3 SOURCE LEAD <liviesor3hq@gmail.com>
2026-05-30 16:55:29 -0400

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-console.log("SUPABASE_URL:", process.env.SUPABASE_URL)
-console.log("SUPABASE_ANON_KEY:", process.env.SUPABASE_ANON_KEY)
-
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
+)
